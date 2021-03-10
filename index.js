8b514d934986baa7e89531d8f21d5750a63b54da
Balazs Zubak <zubak.balazs@gmail.com>
2021-03-10 11:09:37 +0100

    saving data

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const EvohomeClient = require('@svrooij/evohome/lib').EvohomeClient
 const express = require('express')
 var evohomeClient = null;
 
-var username = ''
-var password = ''
+var username = '';
+var password = '';
+
+const fs = require('fs');
+
+try {
+  let rawdata = fs.readFileSync('public/data.json');
+  var data = JSON.parse(rawdata);
+}
+catch (e)
+{
+  var data = [];
+}
+
+
+
 
 const app = express()
 const port = process.env.PORT || 5000;
@@ -36,9 +50,15 @@ function requestData()
     console.log(locations[0].devices[1].name, locations[0].devices[1].thermostat.indoorTemperature);
     console.log(locations[0].devices[2].name, locations[0].devices[2].thermostat.indoorTemperature);
     console.log(locations[0].devices[3].name, locations[0].devices[3].thermostat.indoorTemperature);
-    return {cellar: locations[0].devices[1].thermostat.indoorTemperature,
+    var result = {cellar: locations[0].devices[1].thermostat.indoorTemperature,
             ground: locations[0].devices[2].thermostat.indoorTemperature,
-            first: locations[0].devices[3].thermostat.indoorTemperature};
+            first: locations[0].devices[3].thermostat.indoorTemperature,
+            timestamp: Date.now()};
+
+    data.push(result);
+    fs.writeFileSync('public/data.json', JSON.stringify(data));
+
+    return result;
   }).catch(err => {
     console.log('Error occured %j', err)
   })
